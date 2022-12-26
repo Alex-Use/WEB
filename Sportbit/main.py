d9940f9beb6f017559a920bcc604e8863e44f1c5
Alex-Use <72986039+Alex-Use@users.noreply.github.com>
2022-12-26 10:08:32 +0300

    Add files via upload

--- Sportbit/main.py
+++ Sportbit/main.py
@@ -1,6 +1,17 @@
 import json
 from flask import Flask, jsonify, request, render_template
+import psycopg2
 app = Flask(__name__)
+
+conn = psycopg2.connect(
+    host="localhost",
+    database="watch",
+    user="postgres",
+    password="*******",
+    port=5434
+)
+cursor = conn.cursor()
+success_message = {'success': True}
 
 
 
@@ -11,17 +22,32 @@ def index():
 
 @app.route('/data', methods=['GET'])
 def index():
-    return jsonify(data)
+    data_clients = []
+    sql = 'SELECT * FROM owner'
+    cursor.execute(sql)
+    data = cursor.fetchall()
+    for client in data:
+        data_owners.append({'id': client[0],
+                            'name': client[1],
+                            'surname': client[2],
+                            'phone number': client[3]})
+    print(data_clients)
+    print(request.url)
+    return render_template('index1.html', clients=data_clients)
+
 
 
 @app.route('/data', methods=['POST'])
 def add_user():
-    #for num in range(len(data)):
-    #    if request.get_json()['id'] == data[num]['id']:
-    #        print("ID number is busy")
-    #    else:
-    data.append(request.get_json())
-    return jsonify(data)
+    sql = 'INSERT INTO owner VALUES (%s, %s, %s, %s)'
+    id = random.randint(1, 999999)
+    name = request.get_json()['name']
+    lastname = request.get_json()['lastname']
+    email = request.get_json()['email']
+    cursor.execute(sql, (id, name, lastname, email))
+    conn.commit()
+    return index(id)
+
 
 
 @app.route('/data', methods=['PUT'])
